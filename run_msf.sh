600639f9eb9bce4a1d06f8c496778c8df41f1895
Ajinkya Tejankar <at6@umbc.edu>
2021-08-02 20:28:59 -0700

    fix weak/weak settings

--- run_msf.sh
+++ run_msf.sh
@@ -8,10 +8,10 @@ CUDA_VISIBLE_DEVICES=$1 python train_msf.py \
     --learning_rate 0.05 \
     --epochs 200 \
     --arch resnet50 \
-    --topk 10 \
+    --topk 5 \
     --momentum 0.99 \
-    --mem_bank_size 128000 \
+    --mem_bank_size 1024000 \
     --augmentation 'weak/weak' \
-    --checkpoint_path output/msf_1_ww_resnet50 \
+    --checkpoint_path output/msf_1_ww_topk_5_mbs_1024k_resnet50 \
     /datasets/imagenet
 
